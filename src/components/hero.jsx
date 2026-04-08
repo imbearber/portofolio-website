27818eceb80f0db0ffd12dfb1e5a1dd0c0964f3d
imbearber <ridhoturbo171103@gmail.com>
2026-04-08 09:01:23 +0700

    fixing import image

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,11 @@
 import { TypeAnimation } from 'react-type-animation';
 import { Star } from 'lucide-react'; 
+import photoprofile from '../assets/photo-profile.png'
+import figmalogo from '../assets/figma-logo.svg'
+import sketchlogo from '../assets/sketch-logo.svg'
+import clientone from '../assets/client-1.png'
+import clienttwo from '../assets/client-2.png'
+import clientthree from '../assets/client-3.png'
 
 export default function Hero() {
     return (
@@ -25,8 +31,6 @@ export default function Hero() {
                     <p className="text-txt-secondary text-xl leading-relaxed md:pr-10">
                         Bridging the gap between human centered design and robust technology. I am an Informatics Engineering graduate dedicated to crafting seamless digital products, from conceptualization to functional web execution.
                     </p>
-
-                    {/* Area Tombol */}
                     <div className="flex flex-wrap items-center gap-5 mt-4">
                         <button className="bg-primary text-white font-semibold py-3 px-8 rounded-lg shadow-lg shadow-primary/30 transition-transform duration-300 hover:-translate-y-3">
                             Get In Touch
@@ -35,13 +39,11 @@ export default function Hero() {
                             Book a demo
                         </button>
                     </div>
-
-                    {/* Area Rating */}
                     <div className="flex items-center gap-3 mt-6">
                         <div className="flex -space-x-3">
-                            <img src="/client1.png" alt="Client 1" className="w-10 h-10 rounded-full border-2 border-bg-main object-cover bg-gray-300" />
-                            <img src="/client2.png" alt="Client 2" className="w-10 h-10 rounded-full border-2 border-bg-main object-cover bg-gray-400" />
-                            <img src="/client3.png" alt="Client 3" className="w-10 h-10 rounded-full border-2 border-bg-main object-cover bg-gray-500" />
+                            <img src={clientone} alt="Client 1" className="w-10 h-10 rounded-full border-2 border-bg-main object-cover bg-gray-300" />
+                            <img src={clienttwo} alt="Client 2" className="w-10 h-10 rounded-full border-2 border-bg-main object-cover bg-gray-400" />
+                            <img src={clientthree} alt="Client 3" className="w-10 h-10 rounded-full border-2 border-bg-main object-cover bg-gray-500" />
                         </div>
                         <div className="flex items-center gap-1 text-sm font-semibold text-txt-primary">
                             <span>4.5</span>
@@ -57,7 +59,7 @@ export default function Hero() {
 
                 <div className="relative flex justify-center items-center mt-10 md:mt-0">
                     <div className="w-[300px] h-[300px] md:w-[400px] md:h-[400px] rounded-full overflow-hidden bg-gray-200 relative z-10 shadow-2xl">
-                        <img src="src/assets/photo-profile.png" alt="Berliano Profil" className="w-full h-full object-cover" />
+                        <img src={photoprofile} alt="Berliano Profil" className="w-full h-full object-cover" />
                     </div>
 
                     <div className="absolute top-5 left-0 md:-left-10 z-20 animate-melayang bg-[#FF9161] backdrop-blur-sm text-white px-5 py-2 rounded-full font-semibold shadow-lg">
@@ -69,11 +71,11 @@ export default function Hero() {
                     </div>
 
                     <div className="absolute top-10 right-5 md:-right-8 z-0 animate-melayang [animation-delay:0.5s]">
-                        <img src="src/assets/figma-logo.svg" alt="figma logo" className="w-20 h-20"/> 
+                        <img src={figmalogo} alt="figma logo" className="w-20 h-20"/> 
                     </div>
 
                     <div className="absolute bottom-10 left-5 md:-left-5 z-20 animate-melayang [animation-delay:2s]">
-                        <img src="src/assets/sketch-logo.svg" alt="sketch logo" className="w-20 h-20"/> 
+                        <img src={sketchlogo} alt="sketch logo" className="w-20 h-20"/> 
                     </div> 
                 </div>
                 
